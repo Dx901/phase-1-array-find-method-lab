ed895ed72f390e03e62fbeb134e07e561af237c5
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-21 18:04:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+
+function superbowlWin(array) {
+    for (const record of array) {
+        if (record.result === "W") {
+            return record.year;
+        }
+    }
+
+}
+
+ 
+
